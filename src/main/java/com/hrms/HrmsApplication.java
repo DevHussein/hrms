ff83bdb4d6e7f0fcbac470a73e8bcabfe8c22059
Hussein <hussein_989@hotmail.com>
2018-06-20 01:50:51 +0300

    Commit the JPA changes for the mysql database.

--- src/main/java/com/hrms/HrmsApplication.java
+++ src/main/java/com/hrms/HrmsApplication.java
@@ -2,9 +2,17 @@ package com.hrms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-public class HrmsApplication {
+@EnableJpaAuditing 
+public class HrmsApplication extends SpringBootServletInitializer {
+	   @Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	        return application.sources(HrmsApplication.class);
+	    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(HrmsApplication.class, args);
